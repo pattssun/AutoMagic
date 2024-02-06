a47532cc8bd5fe7a53ca2019427dd375b9a19cda
Patrick Sun <ps4216@nyu.edu>
2024-02-06 13:16:13 -0500

    changed font

--- src/video_processing.py
+++ src/video_processing.py
@@ -16,7 +16,7 @@ def crop_to_916(clip):
     cropped_clip = clip.crop(x1=crop_amount_per_side, y1=0, x2=original_width - crop_amount_per_side, y2=original_height)
     return cropped_clip
 
-def create_text_clip(text, start_time, end_time, fontsize=24, font='resources/fonts/Product Sans Regular.ttf', color='white'):
+def create_text_clip(text, start_time, end_time, fontsize=24, font='arial', color='white'):
     """
     Creates a moviepy TextClip object for a given piece of text.
     """
