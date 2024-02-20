08c37e284bbbc3a1d8f7624f645595c5dcf54f19
Patrick Sun <ps4216@nyu.edu>
2024-02-20 15:37:49 -0500

    refactored codebase

--- main.py
+++ main.py
@@ -1,34 +1,17 @@
 from moviepy.editor import VideoFileClip, AudioFileClip, CompositeVideoClip, ColorClip, concatenate_audioclips
-from video_processing import crop_to_916, create_text_clip_for_body, create_text_clip_for_title
-from audio_processing import speed_up_mp3, text_to_speech
-from text_processing import read_text_file, generate_captions
+from src.video_processing import crop_to_916, create_text_clip_for_body
+from src.audio_processing import speed_up_mp3, text_to_speech
+from src.text_processing import read_text_file, generate_captions
 import random
 from datetime import datetime
 import os
 
-# Hardcoded Reddit banner
-def assemble_video(project_name, voice, background_video_path, title_text, body_text):
+def assemble_video(project_name, voice, background_video_path, body_text):
     """
     Assembles the video from various components, using a pre-rendered image for the title and narrating the title text.
     """
     # Load the background video and crop to a 9:16 aspect ratio
     background_clip = crop_to_916(VideoFileClip(background_video_path))
-
-    # Convert the title text to speech and speed it up
-    normal_title_audio_path = f"resources/audio_files/normal/{project_name} (title_audio.mp3)"
-    faster_title_audio_path = f"resources/audio_files/faster/{project_name} (title_audio.mp3)"
-    if project_name != "TEST":
-        text_to_speech(title_text, voice, normal_title_audio_path) 
-        speed_up_mp3(normal_title_audio_path, faster_title_audio_path, 1.15)
-    title_audio = AudioFileClip(faster_title_audio_path)
-
-    # Create a text clip for the title
-    title_clip = create_text_clip_for_title(title_text, 0, title_audio.duration, clip_size=background_clip.size)
-    
-    # Create a 0.5 second silent audio clip and blank video clip for the pause
-    pause_duration = 0.5
-    pause_audio_clip = AudioFileClip("resources/audio_files/silence.mp3").subclip(0, pause_duration)
-    pause_video_clip = ColorClip(size=title_clip.size, color=(0,0,0,0), duration=pause_duration).set_audio(pause_audio_clip)
 
     # Convert the body text to speech and speed it up
     normal_body_audio_path = f"resources/audio_files/normal/{project_name} (body_audio.mp3)"
@@ -39,16 +22,16 @@ def assemble_video(project_name, voice, background_video_path, title_text, body_
     body_audio = AudioFileClip(faster_body_audio_path)
 
     # Initialize list to hold all video clips
-    video_clips = [title_clip, pause_video_clip]
+    video_clips = []
 
     # Initialize list to hold all audio clips
-    audio_clips = [title_audio, pause_audio_clip]
+    audio_clips = []
 
     # Calculate start and end times for each body caption chunk
     body_captions = generate_captions(faster_body_audio_path)
     for caption in body_captions:
-        start_time = caption['start'] + title_audio.duration + pause_duration
-        end_time = caption['end'] + title_audio.duration + pause_duration
+        start_time = caption['start'] 
+        end_time = caption['end']
         text_clip = create_text_clip_for_body(caption['text'], start_time, end_time, clip_size=background_clip.size)
         video_clips.append(text_clip)
 
@@ -72,37 +55,13 @@ def assemble_video(project_name, voice, background_video_path, title_text, body_
 
     # Remove audio files
     if project_name != "TEST":
-        os.remove(normal_title_audio_path)
-        os.remove(faster_title_audio_path)
         os.remove(normal_body_audio_path)
         os.remove(faster_body_audio_path)
-
-# # Production
-# if __name__ == "__main__":
-#     voice = "Liam" 
-#     background_video_path = "resources/background_videos/minecraft2.mp4" 
-#     today_date = datetime.today().strftime('%Y-%m-%d') # Get today's date
-#     # Assemble the video for each post in today's text_files directory
-#     for i in [1,2,3]:
-#         project_name = f"{today_date}-post{i}" 
-#         title_text = read_text_file(f"resources/text_files/{today_date}/post{i}/title_text.txt")
-#         body_text = read_text_file(f"resources/text_files/{today_date}/post{i}/body_text.txt")
-#         assemble_video(project_name, voice, background_video_path, title_text, body_text)
 
 # Testing
 if __name__ == "__main__":
     voice = "Liam" 
     background_video_path = "resources/background_videos/trackmania.mp4"
     project_name = f"TEST" 
-    title_text = read_text_file(f"resources/text_files/{project_name}/title_text.txt")
-    body_text = read_text_file(f"resources/text_files/{project_name}/body_text.txt")
-    assemble_video(project_name, voice, background_video_path, title_text, body_text)
-
-"""
-Current workflow:
-1. Import 3 Reddit posts in text_files
-2. Download ouput videos
-3. Select TikTok song
-4. Input TikTok description
-5. Upload to TikTok
-"""          
+    body_text = read_text_file("tiktok_test copy.txt")
+    assemble_video(project_name, voice, background_video_path, body_text)
