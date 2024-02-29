0c03d468b2787bfdd8309a64b958bb9f7d539bf6
Patrick Sun <ps4216@nyu.edu>
2024-02-29 16:29:56 -0500

    testing

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ def assemble_video(project_name, background_video_path, body_text_path):
     # Convert the body text to speech and speed it up
     normal_path = f"{project_name}_normal.mp3"
     faster_path = f"{project_name}_faster.mp3"
-    text_to_speech(body_text, f"test/{normal_path}") 
-    speed_up_mp3(f"test/{normal_path}", f"test/{faster_path}", 1.25) 
+    # text_to_speech(body_text, f"test/{normal_path}") 
+    # speed_up_mp3(f"test/{normal_path}", f"test/{faster_path}", 1.25) 
     body_audio = AudioFileClip(f"test/{faster_path}")
 
     # Initialize list to hold all video clips
